493a294ae9904050dace50df8bca355e5266069b
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2024-09-13 14:03:04 +0300

    Leetcode day 5

--- palindrome/index.js
+++ palindrome/index.js
@@ -18,3 +18,51 @@ function palindrome(str) {
     });
 }
 module.exports = palindrome;
+
+/*
+Leetcode 125
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const self = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return self.split('').every((char, i) => {
+        return char === self[self.length - i - 1];
+    });
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama") === true);
+console.log(isPalindrome("race a car") === false);
+console.log(isPalindrome("0P") === false);
